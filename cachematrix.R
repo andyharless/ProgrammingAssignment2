4dbcc4d54a4914a473ec0ca7e57097bf6befecd0
Andy Harless <a23@harless.us>
2017-03-07 12:12:31 -0500

    Completed assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## When a matrix is inverted, cache the inverse 
+## so inverse can be re-used without repeating the inversion
 
-## Write a short comment describing this function
+## makeCacheMatrix: Create an object that wraps a matrix and can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    cached_inverse = NULL
+    set <- function(matrix_to_wrap) {
+        x <<- matrix_to_wrap
+        cached_inverse <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inverse_to_be_cached) cached_inverse <<- inverse_to_be_cached
+    getinv <- function() cached_inverse
+    list( set=set, get=get, setinv=setinv, getinv=getinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve: Get inverse of matrix wrapped in an object returned by makeCacheMatrix.
+##   If cache already exists, cacheSolve will retrieve the inverse from cache.
+##   If no cache exists, cacheSolve will do the inversion and cache the result.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Returns a matrix that is the inverse of the matrix wrapped by argument x
+    the_inverse = x$getinv()
+    if (!is.null(the_inverse)) {
+        message("Cached inverse retrieved.")
+        return(the_inverse) 
+    }
+    the_original_matrix = x$get()
+    the_inverse = solve(the_original_matrix, ...)
+    x$setinv(the_inverse)
+    the_inverse
 }
